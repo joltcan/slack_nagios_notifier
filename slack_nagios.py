f1493213d6478ad8b655591588e40420d095d899
Fredrik Lundhag <fredrik.lundhag@impactct.com>
2024-05-20 09:34:28 +0200

    alertresponse and commiting working code

--- slack_nagios.py
+++ slack_nagios.py
@@ -19,6 +19,7 @@ import os
 SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
 SLACK_APP_TOKEN = os.environ['SLACK_APP_TOKEN']
 
+problems_file = 'problems.json'
 nagios_cmdfile = "/var/lib/nagios4/rw/nagios.cmd"
 
 app = App(token=SLACK_BOT_TOKEN)
@@ -30,6 +31,7 @@ socket_mode_handler = SocketModeHandler(app, SLACK_APP_TOKEN)
 
 def alert_message(data):
     state = data['state']
+    
     # use the bootstrap palette
     # https://www.color-hex.com/color-palette/5452
     if state == "CRITICAL" or state == 'DOWN':
@@ -38,12 +40,16 @@ def alert_message(data):
     elif state == "OK" or state == "UP" or state == "RECOVERY":
         color = '#5cb85c'
         add_ack = False
-    elif state == "WARNING" or state == 'ACKNOWLEDGEMENT':
+    elif state == "WARNING":
         color = '#428bca'
         add_ack = True
     else:
         color = '#cccccc'
         add_ack = False
+
+    if data['type'] == "ACKNOWLEDGEMENT":
+        add_ack = False
+        color = '#428bca'
 
     if 'service' in data:
         notification_message = '''
@@ -53,7 +59,7 @@ IP:\t\t\t\t{ip}
 Service:\t\t{service}
 State:\t\t\t{state}
 '''.format(type=data['type'], host=data['host'], ip=data['ip'], service=data['service'], state=data['state'])
-        value_data = "ACKNOWLEDGE_SVC_PROBLEM;{service};{host}".format(service=data['service_problem_id'], host=data['host'])
+        value_data = "ACKNOWLEDGE_SVC_PROBLEM;{host};{service}".format(service=data['service'], host=data['host'])
     else:
         notification_message = '''
 *<https://nagios.int.impactct.co/nagios4/|Host {type} notification>*
@@ -78,7 +84,7 @@ State:\t\t\t{state}
                         "type": "section",
                         "text": {
                             "type": "mrkdwn",
-                            "text": "*Additional info*\n{info}".format(info=data['info'])
+                            "text": "*Check Output*\n{info}".format(info=data['info'])
                         }
                     },
                 ],
@@ -104,43 +110,56 @@ State:\t\t\t{state}
 
     return response
 
+# ack via slack
+# original message will be updated via nagios with ack_message
 @app.action("ack_message")
 def ack_message_handler(body, ack, say, payload):
     ack()
+    #print(json.dumps(payload, indent=2))
 
     # send acknowledgement to Nagios
     cmdfile = open(nagios_cmdfile, "a")
-    if 'ACKNOWLEDGE_HOST_PROBLEM' in payload['value']:
-        alertresp = "Host Problem notification for %s" % payload['value'].split(';')[1]
-        print("[%d] %s;2;1;0;%s;%s acknowledged via nagios-slack.\n" % (time.time(), payload['value'], body['user']['username'], body['user']['username'] ), file=cmdfile)
-        print("[%d] %s;2;1;0;%s;%s acknowledged via nagios-slack.\n" % (time.time(), payload['value'], body['user']['username'], body['user']['username'] ))
-    elif 'ACKNOWLEDGE_SVC_PROBLEM' in payload['value']:
+    if 'ACKNOWLEDGE_SVC_PROBLEM' in payload['value']:
         alertresp = "Service Problem notification for %s on %s" % (payload['value'].split(';')[1], payload['value'].split(';')[2])
-        print("[%d] %s;2;1;0;%s;%s acknowledged via nagios-slack.\n" % (time.time(), payload['value'], body['user']['username'], body['user']['username'] ), file=cmdfile)
-        print("[%d] %s;2;1;0;%s;%s acknowledged via nagios-slack.\n" % (time.time(), payload['value'], body['user']['username'], body['user']['username'] ))
+        print("[%d] %s;2;1;0;%s;%s acknowledged via %s.\n" % (time.time(), payload['value'], body['user']['username'], body['user']['username'], body['channel']['name'] ), file=cmdfile)
+        print("[%d] %s;2;1;0;%s;%s acknowledged via #%s.\n" % (time.time(), payload['value'], body['user']['username'], body['user']['username'],body['channel']['name'] ))
+    else:
+        alertresp = "Host Problem notification for %s" % payload['value'].split(';')[1]
+        print("[%d] %s;2;1;0;%s;%s acknowledged via %s.\n" % (time.time(), payload['value'], body['user']['username'], body['user']['username'], body['channel']['name'] ), file=cmdfile)
+        print("[%d] %s;2;1;0;%s;%s acknowledged via #%s.\n" % (time.time(), payload['value'], body['user']['username'], body['user']['username'],body['channel']['name'] ))
 
 # ack from Nagios
 def ack_message(data):
     if 'service' in data:
-        if data['service_problem_id'] in problems['service']:
-            alertresp = "Service Problem notification for %s on %s" % (data['service'], data['host'])
-            old_message_ts = problems['service'][data['service_problem_id']]['ts']
-            old_message = problems['service'][data['service_problem_id']]['text']
-            channel = problems['service'][data['service_problem_id']]['channel']
-            # print("Acked %s for %s on %s from nagios" % (data['service_problem_id'], data['service'], data['host']) )
-            del problems['service'][data['service_problem_id']]
+        alertresp = "Service Problem notification for %s on %s" % (data['service'], data['host'])
+        old_message_ts = problems['service'][data['service_problem_id']]['ts']
+        old_message    = problems['service'][data['service_problem_id']]['text']
+        channel = problems['service'][data['service_problem_id']]['channel']
+        print("Acked %s for %s on %s" % (data['service_problem_id'], data['service'], data['host']) )
     else:
-        if data['host_problem_id'] in problems['host']:
-            alertresp = "Host Problem notification for %s" % (data['host'])
+        alertresp = "Host Problem notification for %s" % (data['host'])
+        # recovery for a host has problem_id set to 0, so let's iterate and see if we have it.
+        if data['host_problem_id'] == "0":
+            for problem_id in problems['host']:
+                if data['host'] == problems['host'][problem_id]['host']:
+                    old_message_ts = problems['host'][problem_id]['ts']
+                    old_message    = problems['host'][problem_id]['text']
+                    channel = problems['host'][problem_id]['channel']
+        else:
             old_message_ts = problems['host'][data['host_problem_id']]['ts']
-            old_message = problems['host'][data['host_problem_id']]['text']
+            old_message    = problems['host'][data['host_problem_id']]['text']
             channel = problems['host'][data['host_problem_id']]['channel']
-            # print("Acked %s on %s from nagios" % (data['host_problem_id'], data['host']) )
-            del problems['host'][data['host_problem_id']]
+
+        print("Acked %s on %s" % (data['host_problem_id'], data['host']) )
+
+    if data['type'] == 'RECOVERY' or data['type'] == 'OK' or data['type'] == 'UP':
+        color = '#5cb85c'
+    elif data['type'] == "ACKNOWLEDGEMENT":
+        color = '#428bca'
 
     updated_message = [
             {
-                "color": "#d9534f",
+                "color": color,
                 "blocks": [
                     {
                         "type": "section",
@@ -160,39 +179,71 @@ def ack_message(data):
                 "fallback": alertresp
             }]
 
+    # fugly hack to remove the comment if it's a recovery
+    #if data['host_problem_id'] == "0" or if service in data:
+    #    del(updated_message[0]['blocks'][1])
+
     # https://api.slack.com/methods/chat.update
     app.client.chat_update(channel=channel, attachments=updated_message, ts=old_message_ts)
 
+# Be able to send messages from Flask
 from slack_bolt.context.say import Say
-problems = { "service":{}, "host":{} }
+
+try:
+    with open(problems_file, 'wb') as f:
+        problems = json.load( open(problems_file) )
+except: 
+    problems = { "service":{}, "host":{} }
 
 @flask_app.route("/alertmsg", methods=["POST"])
 def slack_events():
     data = request.get_json(force=True)
     print(json.dumps(data, indent=2))
-    if data['type'] != 'ACKNOWLEDGEMENT':
+    is_cached = False
+
+    if data['type'] not in ['ACKNOWLEDGEMENT', 'RECOVERY']:
         message = app.client.chat_postMessage(channel=data['channel'], attachments=alert_message(data), text=" ")
         # store message timestamp so we can handle ack's from Nagios (no callback, so we need to remember them)
         # host and service problem id can potentially overlap, so needs to be handled separately.
         if 'service' in data:
-            problems['service'][data['service_problem_id']] = {"ts": message['ts'], "text": message['message']['attachments'][0]['blocks'][0]['text']['text'], "channel": message['channel']}
+            problems['service'][data['service_problem_id']] = {"ts": message['ts'], "text": message['message']['attachments'][0]['blocks'][0]['text']['text'], "channel": message['channel'], "host": data['host'] }
         else:
-            problems['host'][data['host_problem_id']] = {"ts": message['ts'], "text": message['message']['attachments'][0]['blocks'][0]['text']['text'], "channel": message['channel']}
-    if data['type'] == 'ACKNOWLEDGEMENT':
+            problems['host'][data['host_problem_id']] = {"ts": message['ts'], "text": message['message']['attachments'][0]['blocks'][0]['text']['text'], "channel": message['channel'], "host": data['host'] }
+
+        # persist the alerts
+        # https://stackoverflow.com/a/55109482
+        with open(problems_file, 'w', encoding='utf-8') as f:
+            json.dump(problems, f, ensure_ascii=False, indent=4)
+
+    else:
         if 'service' in data:
-            if data['service_problem_id'] in problems['service']:
-                message = app.client.chat_postMessage(channel=data['channel'], attachments=ack_message(data), text=" ")
-            else:
-                # send regular ack message if we don't have it cached
+            if data['service_problem_id'] not in problems['service']:
+                # send regular ack message since we dont have the problem in our cache
                 print("Could not find the error %s for %s on host %s in our cache, ignoring." % (data['service_problem_id'], data['service'], data['host']))
                 message = app.client.chat_postMessage(channel=data['channel'], attachments=alert_message(data), text=" ")
-        else:
-            if data['host_problem_id'] in problems['host']:
-                message = app.client.chat_postMessage(channel=data['channel'], attachments=ack_message(data), text=" ")
             else:
-                # send regular ack message if we don't have it cached
+                is_cached = True
+        else:
+            # recovery for a host has problem_id set to 0, so let's iterate and see if we have it.
+            if data['host_problem_id'] == "0":
+                for problem_id in problems['host']:
+                    if data['host'] == problems['host'][problem_id]['host']:
+                        print("found the host by iterating over all host in problems store: %s" % data['host'])
+                        is_cached = True
+            # send regular ack message if we don't have it cached
+            elif data['host_problem_id'] not in problems['host']:
                 print("Could not find the error %s for host %s in our cache, ignoring." % (data['host_problem_id'], data['host']))
                 message = app.client.chat_postMessage(channel=data['channel'], attachments=alert_message(data), text=" ")
+            else:
+                is_cached = True
+        
+        if is_cached: 
+            #print("sending cached ack for %s" % data['type'] )
+            message = app.client.chat_postMessage(channel=data['channel'], attachments=ack_message(data), text=" ")
+       
+        # send a separate recovery message for visibility
+        if data['type'] == 'RECOVERY':
+            message = app.client.chat_postMessage(channel=data['channel'], attachments=alert_message(data), text=" ")
 
     # print(json.dumps(problems, indent=2))
     return "ok\n"
@@ -201,8 +252,9 @@ if __name__ == "__main__":
     socket_mode_handler.connect()  # does not block the current thread
     # Debug/Development
     if 'DEBUG' in os.environ:
-        flask_app.run(port=5000, debug=True)
+        flask_app.run(port=5005, debug=True)
     else:
         print("Starting webserver.")
         http_server = WSGIServer(('', 5000), flask_app)
         http_server.serve_forever()
+
